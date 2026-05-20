b7d9b2735870b318f38e75d088026dd50fc74e03
adimerpaul <adimer101@gmail.com>
2026-05-20 00:49:08 -0400

    Actulzido unidada de medida

--- back/app/Models/User.php
+++ back/app/Models/User.php
@@ -35,8 +35,6 @@ class User extends Authenticatable implements Auditable
         'area_id',
         'establecimiento_id',
         'unidad_id',
-        'admisiones',
-        'personal_turnos',
     ];
 
     /**
--- front/dist/pwa/assets/use-quasar-DlOQENLm.js
+++
@@ -1,1 +0,0 @@
-import{z as a,aK as r}from"./index-7Y5Ce-PD.js";function u(){return a(r)}export{u};
