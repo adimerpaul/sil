9cee286762b089cf025e3775e6efa9ed604bcbd6
Adimer Paul Chambi Ajata <6387376-adimerpaul@users.noreply.gitlab.com>
2026-08-05 15:25:58 -0400

    Actulizado para altos y bajos

--- front/dist/pwa/assets/use-quasar-Ms5JUg5R.js
+++
@@ -1,1 +0,0 @@
-import{z as a,bC as r}from"./index-D3pwk2II.js";function u(){return a(r)}export{u};
---
+++ front/dist/pwa/assets/use-quasar-_Po-QgtO.js
@@ -0,0 +1,1 @@
+import{z as a,bC as r}from"./index-D20-6NUx.js";function u(){return a(r)}export{u};
